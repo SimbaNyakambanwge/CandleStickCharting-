3dc60b6a895de43a596b70117474812dcf64b9d4
Simbarashe Nyakambangwe <nyakambs@uni.coventry.ac.uk>
2019-11-18 13:19:56 +0000

    Finance Director Class

--- CandlestickCharting/Candlestick.h
+++ CandlestickCharting/Candlestick.h
@@ -1,14 +1,30 @@
 #pragma once
+#ifndef candlestick_H
+#define candlestick_H
+#endif // !candlestick_H
+#include <string>
+#include <vector>
+using namespace std;
+
 class CandlestickChart
 {
 private: 
+	
+public:
+	
 
 };
 
 class FinanceDirector
 {
+private:
+	 string director;
+
 public:
+	FinanceDirector();
+		void directorfiles();
 };
+
 class Candle
 {
 private:
